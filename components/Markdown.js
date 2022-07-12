8b3709d367263af01371743e57dfc38f4b21defb
bbangjooo <airmancho@naver.com>
2022-07-12 13:17:28 +0900

    header fix

--- components/Markdown.js
+++ components/Markdown.js
@@ -1,6 +1,6 @@
 import ReactMarkdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { style } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { style } from 'react-syntax-highlighter/dist/esm/styles/prism/prism';
 export default function Markdown({markdown, remarkPlugins, components}) {
     console.log("hi");
     for (const props in style) {
