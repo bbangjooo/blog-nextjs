c0fdaad1f0a8c727547c890dcf20785c23922794
bbangjooo <airmancho@naver.com>
2022-07-12 13:31:22 +0900

    header fix

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -39,8 +39,8 @@ const customComponents = {
             const image = node.children[0]
             const metaWidth = image.properties.alt.match(/{([^}]+)x/)
             const metaHeight = image.properties.alt.match(/x([^}]+)}/)
-            const width = metaWidth ? metaWidth[1] : "100%"
-            const height = metaHeight ? metaHeight[1] : "100%"
+            const width = metaWidth ? metaWidth[1] : "auto"
+            const height = metaHeight ? metaHeight[1] : "auto"
             return (
                 <div className="wrap">
                     <Image
